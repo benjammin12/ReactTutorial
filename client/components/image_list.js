419511de5bc25e032f2a731974f0ef960be9a089
benjammin12 <benxerri@gmail.com>
2016-08-16 22:41:04 -0400

    Added component

---
+++ client/components/image_list.js
@@ -0,0 +1,15 @@
+//create a component folder and create a js file for each component of project
+
+import React from 'react';
+
+const ImageList = () => {
+  return (
+    <ul>
+      <li> </li>
+    </ul>
+  );
+};
+
+export default ImageList;
+
+//Create other 2 components.
